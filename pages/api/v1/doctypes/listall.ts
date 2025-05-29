5adcb2a690456e1c4c9dec42eb6bc1c3bd1d5d03
Luis Vidal <luvidal@edictus.com>
2025-05-29 13:05:22 -0400

    removed unnecesary files

--- pages/api/v1/doctypes/listall.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { execute } from '../helpers'
-
-async function listall(req: NextApiRequest, res: NextApiResponse) {
-    const doctypes =  await execute('_hooks.sp_get_doctypes_all');
-    return res.status(200).json(doctypes);
-}
-
-export default listall;
